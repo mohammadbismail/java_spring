fa564d4a67ea3a83cc3ad286a5b9e6ce581fe259
mohammadbismail <m.basim.ismail@gmail.com>
2022-10-31 13:16:17 +0200

    sub

--- DaikinchiPathVar/src/main/java/com/axsos/spring/HomeController.java
+++ DaikinchiPathVar/src/main/java/com/axsos/spring/HomeController.java
@@ -10,23 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping ("/daikichi")
 public class HomeController {
 	
-	@RequestMapping(value = "/{travel}/{somecity}", method = RequestMethod.GET)
+	@RequestMapping(value = "/travel/{somecity}", method = RequestMethod.GET)
 	@ResponseBody
-	public String travel(@PathVariable("travel") String travel,@PathVariable("somecity") String city) {
+	public String travel(@PathVariable("somecity") String city) {
 		return "Congratulations! You will soon travel to " + city;
 	}
-//	@RequestMapping(value = "/{lotto}/{num}", method = RequestMethod.GET)
-//	@ResponseBody
-//	public String lotto(@PathVariable String lotto,@PathVariable String num) {
-//		Integer number = Integer.parseInt(num);
-//		if (number % 2 == 0) {
-//			return "You will take a ground journey in the near future, but be weary of tempting offers!";			
-//		}
-//		else {
-//			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";			
-//
-//		}
-//	}
+	@RequestMapping(value = "/{lotto}/{num}", method = RequestMethod.GET)
+	@ResponseBody
+	public String lotto(@PathVariable("lotto") String lotto,@PathVariable("num") String num) {
+		Integer number = Integer.parseInt(num);
+		if (number % 2 == 0) {
+			return "You will take a ground journey in the near future, but be weary of tempting offers!";			
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";			
+
+		}
+	}
 }
+	
+
 
 

0c7956c2c039786a14a5ad05d2a54828ff820a33
mohammadbismail <m.basim.ismail@gmail.com>
2022-10-31 14:06:09 +0200

    hellohuman sensi

--- HelloHuman/src/main/java/com/axsos/spring/HomeController.java
+++ HelloHuman/src/main/java/com/axsos/spring/HomeController.java
@@ -1,19 +1,33 @@
 package com.axsos.spring;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/")
 public class HomeController {
-	@RequestMapping("/")
-	public String index(@RequestParam(value="name", required = false) String searchRes) {
-		if (searchRes == null ) {
-			return "Hello Human";
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(@RequestParam Map<String,String> requestParams) throws Exception {
+		String firstName = requestParams.get("name");
+		String lastName = requestParams.get("last");
+//		Integer times = Integer.parseInt(requestParams.get("times"));
+		
+			if (requestParams.isEmpty()) {
+//				for (int i = 0; i < times ; i++)					
+					return "Hello Human";
+			}
+			else if (lastName.isEmpty()) {
+//				for (int i = 0; i < times ; i++) 			
+				return "Hello "+firstName; 
 		}
-		else {
-		return "Hello "+searchRes; 
+			else {
+//				for (int i = 0; i < times ; i++)
+				return "Hello " + firstName + lastName;
 		}
-	}
+			
+    }
 }
+

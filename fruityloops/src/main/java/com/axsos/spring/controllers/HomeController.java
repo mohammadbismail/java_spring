a6e574af85282d96f2f2347def34b761356e31fa
mohammadbismail <m.basim.ismail@gmail.com>
2022-11-01 13:45:11 +0200

    fruits

---
+++ fruityloops/src/main/java/com/axsos/spring/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package com.axsos.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.axsos.spring.Item;
+
+import java.util.ArrayList;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index(Model model) {
+		
+		ArrayList<Item> fruits = new ArrayList<Item>();
+		fruits.add(new Item("Kiwi", 1.5));
+		fruits.add(new Item("Mango", 2.0));
+		fruits.add(new Item("Goji Berries", 4.0));
+		fruits.add(new Item("Guava", .75));
+		model.addAttribute("keyFruits", fruits );
+		
+		return "index.jsp";
+	}
+}
--- fruityloops/src/main/webapp/WEB-INF/index.jsp
+++ fruityloops/src/main/webapp/WEB-INF/index.jsp
@@ -1,12 +1,27 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
+<meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
-
+	<h1>Fruits Store</h1>
+	<table>
+		<thead>
+		<tr>
+		<td>Price</td>
+		<td>Name</td>
+		</tr>
+		</thead>
+		<c:forEach var="item" items="${keyFruits}">
+		<tr>
+		<td><c:out value="${item.getName()}"/></td>
+		<td><c:out value="${item.getPrice()}"/></td>
+		</tr>
+		</c:forEach>
+	</table>
 </body>
 </html>
